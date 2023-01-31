1e688716bcb5ced73bd0eb01f6c970e80b67f9e3
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-01-31 17:21:55 +0200

    add mobile filter

---
+++ js/mobileFilter.js
@@ -0,0 +1,17 @@
+(function () {
+
+    const btnOpen = document.querySelector(".filter-open")
+    const btnClose = document.querySelector(".filter-close")
+    const filter = document.getElementById("filter")
+
+
+    btnOpen.addEventListener("click", () => {
+        filter.style.display = "block"
+    })
+
+    btnClose.addEventListener("click", () => {
+        filter.style.display = "none"
+    })
+
+
+})()
